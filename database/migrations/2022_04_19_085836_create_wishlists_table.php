b1859693745e7e5c21474fb240ce898ca7a55665
Michael Stanley <michstan96@gmail.com>
2022-04-19 16:02:24 +0700

    add create wishlist table migration

---
+++ database/migrations/2022_04_19_085836_create_wishlists_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWishlistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wishlists', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('usersbook_id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('usersbook_id')->references('id')->on('users_books');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wishlists');
+    }
+}
