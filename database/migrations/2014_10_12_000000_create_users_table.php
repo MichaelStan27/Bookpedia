e4a743b93c71e045233a2fd4b9cbc842c7638178
Michael Stanley <michstan96@gmail.com>
2022-04-19 15:13:14 +0700

    edit user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone_number');
+            $table->string('city');
+            $table->integer('password');
+            $table->text('detail_address');
+            $table->decimal('balance');
             $table->rememberToken();
             $table->timestamps();
         });
