8baaf128db185871f9266e37d789a8ab841e7197
Michael Stanley <michstan96@gmail.com>
2022-05-22 09:47:10 +0700

    Make user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder {
@@ -11,6 +12,20 @@ class UserSeeder extends Seeder {
      * @return void
      */
     public function run() {
-        //
+        User::insert([
+            [
+                'first_name' => 'Budi',
+                'last_name' => 'Sutanto',
+                'email' => 'budi@gmail.com',
+                'password' => 'test123',
+                'phone_number' => '081579468812',
+                'city' => 'Jakarta Selatan',
+                'postal_code' => '10250',
+                'detail_address' => 'Jalan Pulo Raya V No.14',
+                'balance' => '1500000',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
