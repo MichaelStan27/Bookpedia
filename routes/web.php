3110d49dd8a5e71c86149a64c5023be1bc78b849
pascalwilman28 <pascalwilman87@gmail.com>
2022-06-14 16:09:08 +0700

    Profile Commit first

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ViewControllers;
 
@@ -28,3 +29,5 @@ Route::controller(AccountController::class)->group(function () {
 Route::get('/add-book', function(){
     return view('add-book');
 });
+
+Route::get('/profile', [ProfileController::class, 'profile']);
