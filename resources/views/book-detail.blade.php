5f5cf431059f5787e012bedb4300c2a26f5996f7
Exaltation <michstan96@gmail.com>
2022-06-23 10:31:20 +0700

    Implement load book from database in book-detail -make book-user-card component -send variable from controller

--- resources/views/book-detail.blade.php
+++ resources/views/book-detail.blade.php
@@ -6,33 +6,31 @@
     <div class="container mx-auto flex justify-center mb-4">
         <div class="bg-white shadow-md rounded-md border-2 py-4 w-3/4">
             <div class="flex justify-around w-full gap-5 mx-auto py-2 px-8">
-                <img src="{{ asset('assets/books/naruto - vol 1/cover.jpg') }}" alt="" class="w-1/4">
+                <img src="{{ asset("assets/$book->image") }}" alt="" class="w-1/4">
                 <div class="w-3/4 p-2 h-full">
                     <div class="flex justify-between">
-                        <h1 class="font-bold text-3xl">Book Title</h1>
+                        <h1 class="font-bold text-3xl">{{ $book->title }}</h1>
                         <form action="" method="post">
                             @csrf
-                            <div class="hover:scale-105">
-                                <button type="submit" class="relative w-[30px] h-[30px] rotate-45 shadow-md bg-gray-300">
-                                    <div class="absolute w-full h-full top-0 left-[-50%] rounded-full bg-gray-300">
-                                    </div>
-                                    <div class="absolute w-full h-full top-[-50%] rounded-full bg-gray-300">
-                                    </div>
+                            <div class="hover:scale-105 hover:text-red-500">
+                                <button type="submit" class="">
+                                    <i class="fa-solid fa-heart fa-2xl"></i>
                                 </button>
                             </div>
                         </form>
                     </div>
 
-                    <h2 class="font-medium text-xl text-gray-500 mb-3">Category</h2>
-                    <h3 class="rounded-md bg-gray-300 pt-1 px-2 align-text-top text-sm mb-4 h-48">Summary</h3>
+                    <h2 class="font-medium text-xl text-gray-500 mb-3">{{ $book->category->category_name }}</h2>
+                    <h3 class="rounded-md bg-gray-300 pt-1 px-2 align-text-top text-sm mb-4 h-48">{{ $book->summary }}
+                    </h3>
                     <div class="grid grid-cols-2 divide-x-2">
                         <div class="text-center">
                             <h1 class="text-md font-medium">Rent</h1>
-                            <h2 class="text-sm text-gray-500">Rp 20.000</h2>
+                            <h2 class="text-sm text-gray-500">{{ $book->loan_price_with_notation }}</h2>
                         </div>
                         <div class="text-center">
                             <h1 class="text-md font-medium">Sell</h1>
-                            <h2 class="text-sm text-gray-500">Rp 150.000</h2>
+                            <h2 class="text-sm text-gray-500">{{ $book->sale_price_with_notation }}</h2>
                         </div>
                     </div>
                 </div>
@@ -44,26 +42,26 @@
                     <table class="text-sm">
                         <tr class="">
                             <td class="font-semibold">Tahun Terbit</td>
-                            <td class="">: dd M yyyy</td>
+                            <td class="">: {{ $book->released_year }}</td>
                         </tr>
                         <tr class="">
                             <td class="font-semibold">Penerbit</td>
-                            <td class="">: David Beckham</td>
+                            <td class="">: {{ $book->publisher }}</td>
                         </tr>
                         <tr class="">
-                            <td class="font-semibold">ISBN</td>
-                            <td class="">: ISBN 978-602-8519-93-9</td>
+                            <td class="font-semibold">Added</td>
+                            <td class="">: {{ $book->release_date }}</td>
                         </tr>
                         <tr class="">
                             <td class="font-semibold">Author</td>
-                            <td class="">: Budi</td>
+                            <td class="">: {{ $book->author }}</td>
                         </tr>
 
                     </table>
                 </div>
                 <div class="bg-gray-300 rounded-r-md py-2 px-4">
                     <h1 class=" font-bold text-md mb-4">Description</h1>
-                    <p class="">Ini tempat teks deskripsi ditaruh</p>
+                    <p class="">{{ $book->description }}</p>
                 </div>
             </div>
 
@@ -81,35 +79,9 @@
         <div class="py-4 w-3/4">
             <h1 class="text-xl font-bold text-gray-500 mb-4">From This User</h1>
             <div class="grid grid-cols-4 gap-4 mb-2">
-                <div class="rounded-lg border-2 shadow-md overflow-hidden hover:scale-105">
-                    <img src="{{ asset('assets/books/naruto - vol 2/cover.jpg') }}" alt="">
-                    <div class="p-2">
-                        <h1 class="font-bold text-xl">Book Title</h1>
-                        <p class="font-semibold text-md text-gray-500">Rp. xxx.xxx</p>
-                    </div>
-                </div>
-                <div class="rounded-lg border-2 shadow-md overflow-hidden hover:scale-105">
-                    <img src="{{ asset('assets/books/naruto - vol 2/cover.jpg') }}" alt="">
-                    <div class="p-2">
-                        <h1 class="font-bold text-xl">Book Title</h1>
-                        <p class="font-semibold text-md text-gray-500">Rp. xxx.xxx</p>
-                    </div>
-                </div>
-                <div class="rounded-lg border-2 shadow-md overflow-hidden hover:scale-105">
-                    <img src="{{ asset('assets/books/naruto - vol 2/cover.jpg') }}" alt="">
-                    <div class="p-2">
-                        <h1 class="font-bold text-xl">Book Title</h1>
-                        <p class="font-semibold text-md text-gray-500">Rp. xxx.xxx</p>
-                    </div>
-                </div>
-                <div class="rounded-lg border-2 shadow-md overflow-hidden hover:scale-105">
-                    <img src="{{ asset('assets/books/naruto - vol 2/cover.jpg') }}" alt="">
-                    <div class="p-2">
-                        <h1 class="font-bold text-xl">Book Title</h1>
-                        <p class="font-semibold text-md text-gray-500">Rp. xxx.xxx</p>
-                    </div>
-                </div>
-
+                @foreach ($books_from_user as $bookUser)
+                    <x-book-user-card :book="$bookUser"> </x-book-user-card>
+                @endforeach
             </div>
             <div class="text-right">
                 <a href="" class="underline text-md text-gray-500 hover:text-gray-700">See More...</a>
@@ -121,34 +93,9 @@
         <div class="py-4 w-3/4">
             <h1 class="text-xl font-bold text-gray-500 mb-4">Related Books</h1>
             <div class="grid grid-cols-4 gap-4 mb-2">
-                <div class="rounded-lg border-2 shadow-md overflow-hidden hover:scale-105">
-                    <img src="{{ asset('assets/books/naruto - vol 2/cover.jpg') }}" alt="">
-                    <div class="p-2">
-                        <h1 class="font-bold text-xl">Book Title</h1>
-                        <p class="font-semibold text-md text-gray-500">Rp. xxx.xxx</p>
-                    </div>
-                </div>
-                <div class="rounded-lg border-2 shadow-md overflow-hidden hover:scale-105">
-                    <img src="{{ asset('assets/books/naruto - vol 2/cover.jpg') }}" alt="">
-                    <div class="p-2">
-                        <h1 class="font-bold text-xl">Book Title</h1>
-                        <p class="font-semibold text-md text-gray-500">Rp. xxx.xxx</p>
-                    </div>
-                </div>
-                <div class="rounded-lg border-2 shadow-md overflow-hidden hover:scale-105">
-                    <img src="{{ asset('assets/books/naruto - vol 2/cover.jpg') }}" alt="">
-                    <div class="p-2">
-                        <h1 class="font-bold text-xl">Book Title</h1>
-                        <p class="font-semibold text-md text-gray-500">Rp. xxx.xxx</p>
-                    </div>
-                </div>
-                <div class="rounded-lg border-2 shadow-md overflow-hidden hover:scale-105">
-                    <img src="{{ asset('assets/books/naruto - vol 2/cover.jpg') }}" alt="">
-                    <div class="p-2">
-                        <h1 class="font-bold text-xl">Book Title</h1>
-                        <p class="font-semibold text-md text-gray-500">Rp. xxx.xxx</p>
-                    </div>
-                </div>
+                @foreach ($related_books as $related_book)
+                    <x-book-user-card :book="$related_book"> </x-book-user-card>
+                @endforeach
             </div>
             <div class="text-right">
                 <a href="" class="underline text-md text-gray-500 hover:text-gray-700">See More...</a>
---
+++ resources/views/components/book-user-card.blade.php
@@ -0,0 +1,38 @@
+@props(['book'])
+
+<a href="{{ route('book-detail', $book) }}">
+    <div class="rounded-lg border-2 shadow-md overflow-hidden hover:scale-105">
+        <img src="{{ asset("assets/$book->image") }}" alt="{{ $book->title }}" class="">
+        <div class="p-2">
+            <h1 class="font-bold text-xl">{{ Str::limit($book->title, 15, $end = '...') }}</h1>
+            @switch($book->transaction->id)
+                @case(1)
+                    <div class="">
+                        <p class="font-bold text-md text-gray-600">Loan</p>
+                        <p class="font-semibold text-md text-gray-500">{{ $book->loan_price_with_notation }}</p>
+                    </div>
+                @break
+
+                @case(2)
+                    <div class="">
+                        <p class="font-bold text-md text-gray-600">Sale</p>
+                        <p class="font-semibold text-md text-gray-500">{{ $book->sale_price_with_notation }}</p>
+                    </div>
+                @break
+
+                @case(3)
+                    <div class="grid grid-cols-2 divide-x-2">
+                        <div class="bottom-0">
+                            <p class="font-bold text-md text-gray-600">Loan</p>
+                            <p class="font-semibold text-md text-gray-500">{{ $book->loan_price_with_notation }}</p>
+                        </div>
+                        <div class="bottom-0">
+                            <p class="font-bold text-md text-gray-600">Sale</p>
+                            <p class="font-semibold text-md text-gray-500">{{ $book->sale_price_with_notation }}</p>
+                        </div>
+                    </div>
+                @break
+            @endswitch
+        </div>
+    </div>
+</a>
