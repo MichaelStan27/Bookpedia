79ee0d2a601caa3f5232af5cd620a61b9edeeb6e
Daniel <60413613+arville27@users.noreply.github.com>
2022-06-26 13:09:31 +0700

    Improve book card component - Add manage book button for owner - Resize image - Remove divider (add gap)

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -53,7 +53,7 @@
 
         {{-- MyBook Section --}}
         <hr class="mt-2 mb-2">
-        <div class="flex flex-wrap justify-between items-center">
+        <div id="mybook" class="flex flex-wrap justify-between items-center">
             <div class="text-2xl font-bold">MY BOOK</div>
             <a href="/add-book/"
                 class="inline-block px-6 py-3 bg-blue-400 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-500 hover:shadow-lg focus:bg-blue-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-600 active:shadow-lg transition duration-150 ease-in-out">Add
