8ced0f2980e2e75eacf4cea7f3e5addf8e5c3538
Daniel <60413613+arville27@users.noreply.github.com>
2022-06-26 16:55:15 +0700

    Improve dashboard page - Add eager loading to query

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller {
     public function index(Request $request) {
         return view('dashboard', [
-            'books' => Book::limit(10)->get(),
+            'books' => Book::with(['transaction', 'category'])->limit(10)->get(),
             'users' => User::limit(5)->get()
         ]);
     }
