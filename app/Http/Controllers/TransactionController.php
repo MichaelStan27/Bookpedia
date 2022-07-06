2ae3e32fb4902b25cce4851c93cd33ec80b37174
Daniel <60413613+arville27@users.noreply.github.com>
2022-07-06 22:02:09 +0700

    Update checkout logic to adapt new database change

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,62 +4,70 @@ namespace App\Http\Controllers;
 
 use App\Models\BuyTransaction;
 use App\Models\CartItem;
-use App\Models\LoanTransaction;
+use App\Models\LoanDetails;
+use App\Models\Transaction;
 use App\Models\User;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Request;
 
 class TransactionController extends Controller {
     public function checkout(Request $request) {
-        $user = User::find(auth()->user()->id);
-        
-        if ($user->balance < $request->total) {
-            return redirect()->route("cart")->with('checkout_fails', 'Insufficient Balance!</b>');
+        $user = auth()->user();
+
+        $cartItems = $user->cartItems;
+
+        $cartTotal = $cartItems->reduce(function ($carry, $item) {
+            if ($item->type_id == 1) {
+                return $carry + $item->book->loan_price * $item->duration;
+            } else {
+                return $carry + $item->book->sale_price;
+            }
+        });
+
+        $user->update([
+            'balance' => ($user->balance - $cartTotal)
+        ]);
+
+        if ($user->balance < $cartTotal) {
+            return redirect()->route("cart")->with('message', '<b>Insufficient Balance!</b>');
         }
 
-        $cartItems = CartItem::where('user_id', $user->id)->get();
-
         foreach ($cartItems as $cartItem) {
-            $seller = User::find($cartItem->book->user_id);
-            $user->update([
-                'balance' => ($user->balance - $request->total)
-            ]);
+            $seller = $cartItem->book->user;
+
             $date = Carbon::now();
+
+            $transaction = Transaction::create([
+                'book_id' => $cartItem->book_id,
+                'user_id' => $cartItem->user_id,
+                'type_id' => $cartItem->type_id
+            ]);
+
             if ($cartItem->type_id == 1) {
                 $seller->update([
                     'balance' => ($seller->balance + $cartItem->book->loan_price)
                 ]);
-                $transaction = new LoanTransaction();
-                $transaction->book_id = $cartItem->book_id;
-                $transaction->user_id = $cartItem->user_id;
-                $transaction->deadline = $date->addWeeks($cartItem->duration);
-                $transaction->loan_date = now();
-                $transaction->return_date = NULL;
-                $transaction->duration = $cartItem->duration;
+
+                LoanDetails::create([
+                    'transaction_id' => $transaction->id,
+                    'deadline' => $date->addWeeks($cartItem->duration),
+                    'loan_date' => $date,
+                    'return_date' => NULL,
+                    'duration' => $cartItem->duration,
+                ]);
+
                 $cartItem->book->update([
                     'status_id' => 2
                 ]);
-                $transaction->save();
-                $cartItem->delete();
             } else {
                 $seller->update([
                     'balance' => ($seller->balance + $cartItem->book->sale_price)
                 ]);
-                $transaction = new BuyTransaction();
-                $transaction->book_id = $cartItem->book_id;
-                $transaction->user_id = $cartItem->user_id;
-                $transaction->created_at = now();
-                $transaction->updated_at = now();
-                $transaction->save();
-                $cartItem->delete();
-
-                // DB::delete('delete from books where id = ?', [$transaction->book_id]);
             }
+
+            $cartItem->delete();
         }
 
-
-
-        return redirect()->route("cart")->with('message', $request->count . ' book(s) has successfully been checked out!<br><b>Your total Payment : ' . $request->total . '</b><br> Your Balance: ' . $user->balance . '</b>');
+        return redirect()->route("cart")->with('message', $cartItems->count() . ' book(s) has successfully been checked out!<br><b>Your total Payment : ' . $cartTotal . '</b><br> Your Balance: ' . $user->balance . '</b>');
     }
 }
