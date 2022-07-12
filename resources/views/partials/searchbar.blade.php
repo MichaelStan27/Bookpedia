e40d1ac454c34270d2f1def774760a0a9652715d
Yohanes <yohanesabing@gmail.com>
2022-07-12 19:13:58 +0700

    Fix bug in search bar

--- resources/views/partials/searchbar.blade.php
+++ resources/views/partials/searchbar.blade.php
@@ -1,6 +1,6 @@
 <div>
     <form action="{{ route('search') }}" id="searchForm">
-        <div class="rounded-full bg-gray-100 text-base py-3 px-5">
+        <div class="w-fit rounded-full bg-gray-100 text-base py-3 px-5">
             <input type="text" name="keyword" id="query" placeholder="Cari Produk, Judul Buku, Penulis"
                 autocomplete="off" class="bg-gray-100 outline-none w-64">
         </div>
