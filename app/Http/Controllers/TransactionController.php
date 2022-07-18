8e1262d41d76529bba3138870e7b7e7cdc0dc29b
Daniel <60413613+arville27@users.noreply.github.com>
2022-07-18 16:09:35 +0700

    Refactor TransactionController - Merge TransactionController and TransactionManagementController

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -6,12 +6,12 @@ use App\Models\DetailTransaction;
 use App\Models\HeaderTransaction;
 use App\Models\LoanDetails;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 
 class TransactionController extends Controller {
     public function checkout(Request $request) {
         $user = auth()->user();
-        
+
         //Check for validate cart items
         $trashes = $user->cartItemsTrashed()
             ->with('book')->get('cart_items.*');
@@ -20,9 +20,9 @@ class TransactionController extends Controller {
             ->with('book')
             ->join('books', 'book_id', 'books.id')
             ->where('status_id', '2')
-            ->get('cart_items.*');  
+            ->get('cart_items.*');
 
-        if(!$trashes->isEmpty() || !$borrowed->isEmpty()){
+        if (!$trashes->isEmpty() || !$borrowed->isEmpty()) {
             return redirect()->back();
         }
 
@@ -79,14 +79,11 @@ class TransactionController extends Controller {
                 $cartItem->book->update([
                     'status_id' => 2
                 ]);
-
-                $cartItem->delete();
             } else {
                 $seller->update([
                     'balance' => ($seller->balance + $cartItem->book->sale_price)
                 ]);
                 $cartItem->book()->delete();
-                $cartItem->delete();
             }
 
             $cartItem->delete();
@@ -95,5 +92,139 @@ class TransactionController extends Controller {
         return redirect()
             ->back()
             ->with('message', $cartItems->count() . ' book(s) has successfully been checked out!<br>Your total Payment :<b> IDR ' . number_format($cartTotal) . '</b><br> Your Balance:<b> IDR  ' . number_format($user->balance) . '</b>');
+    }
+
+    public function orders(Request $request) {
+
+        $target = $request->query('tab') ?? 'ondelivery';
+
+        $user = auth()->user();
+
+        switch ($target) {
+            case 'finish':
+                $orders = $user->buyHeaderTransactions()
+                    ->with(['deliveryStatus', 'seller', 'buyer'])
+                    ->where('header_transactions.delivery_status_id', '=', 3)
+                    ->orderBy('created_at', 'DESC')
+                    ->get();
+                break;
+
+            case 'onloan':
+                $orders = DetailTransaction::with(['loanDetails', 'headerTransaction', 'book', 'transactionType'])
+                    ->join('header_transactions', 'detail_transactions.header_transaction_id', '=', 'header_transactions.id')
+                    ->join('loan_details', 'loan_details.detail_transaction_id', '=', 'detail_transactions.id')
+                    ->where('header_transactions.buyer_id', '=', $user->id)
+                    ->whereBetween('loan_details.delivery_status_id', [4, 5])
+                    ->orderBy('created_at', 'DESC')
+                    ->get('detail_transactions.*');
+                break;
+
+            case 'ondelivery':
+            default:
+                $orders = $user->buyHeaderTransactions()
+                    ->with(['deliveryStatus', 'seller', 'buyer'])
+                    ->where('header_transactions.delivery_status_id', '<', 3)
+                    ->orderBy('created_at', 'DESC')
+                    ->get();
+                break;
+        }
+
+        return view('my-orders', ['groupedOrders' => $orders, 'target' => $target]);
+    }
+
+    public function sales(Request $request) {
+
+        $target = $request->query('tab') ?? 'ondelivery';
+
+        $user = auth()->user();
+
+        switch ($target) {
+            case 'finish':
+                $sales = $user->sellHeaderTransactions()
+                    ->with(['deliveryStatus', 'seller', 'buyer'])
+                    ->where('header_transactions.delivery_status_id', '=', 3)
+                    ->orderBy('created_at', 'DESC')
+                    ->get();
+                break;
+
+            case 'onloan':
+                $sales = DetailTransaction::with(['loanDetails', 'headerTransaction', 'book', 'transactionType'])
+                    ->join('header_transactions', 'detail_transactions.header_transaction_id', '=', 'header_transactions.id')
+                    ->join('loan_details', 'loan_details.detail_transaction_id', '=', 'detail_transactions.id')
+                    ->where('header_transactions.seller_id', '=', $user->id)
+                    ->whereBetween('loan_details.delivery_status_id', [4, 5])
+                    ->orderBy('created_at', 'DESC')
+                    ->get('detail_transactions.*');
+                break;
+
+            case 'ondelivery':
+            default:
+                $sales = $user->sellHeaderTransactions()
+                    ->with(['deliveryStatus', 'seller', 'buyer'])
+                    ->where('header_transactions.delivery_status_id', '<', 3)
+                    ->orderBy('created_at', 'DESC')
+                    ->get();
+                break;
+        }
+
+        return view('my-sales', ['groupedSales' => $sales, 'target' => $target]);
+    }
+
+    public function updateSaleHeader(HeaderTransaction $headerTransaction) {
+        $user = auth()->user();
+
+        if ($headerTransaction->deliveryStatus->is('being packaged') && $headerTransaction->seller->id != $user->id) {
+            abort(403);
+        } else if ($headerTransaction->deliveryStatus->is('shipped') && $headerTransaction->buyer->id != $user->id) {
+            abort(403);
+        }
+
+        if ($headerTransaction->incrementDeliveryStatus()) {
+            if ($headerTransaction->deliveryStatus->is('shipped')) {
+                $headerTransaction
+                    ->getLoanTransaction()
+                    ->each(function ($item) {
+                        $loanInfo = $item->loanDetails;
+                        $loanInfo->update([
+                            'delivery_status_id' => $loanInfo->delivery_status_id + 1,
+                            'loan_date' => Carbon::now(),
+                            'deadline' => Carbon::now()->addWeeks($loanInfo->duration)
+                        ]);
+                    });
+            }
+            $message = 'Item\'s status successfully updated';
+        } else {
+            $message = 'Cannot update item\'s status';
+        }
+
+        return redirect()->back()->with('message', $message);
+    }
+
+    public function updateSaleItem(DetailTransaction $transaction) {
+        $user = auth()->user();
+
+        $headerTransaction = $transaction->headerTransaction;
+
+        if ($transaction->loanDetails->deliveryStatus->is('shipped back') && $headerTransaction->seller->id != $user->id) {
+            abort(403);
+        } else if ($transaction->loanDetails->deliveryStatus->is('loan') && $headerTransaction->buyer->id != $user->id) {
+            abort(403);
+        }
+
+        if ($transaction->loanDetails->incrementDeliveryStatus()) {
+            if ($transaction->loanDetails->deliveryStatus->is('shipped back')) {
+                $transaction->loanDetails->update([
+                    'return_date' => Carbon::now()
+                ]);
+                $transaction->book->update([
+                    'status_id' => 1
+                ]);
+            }
+            $message = 'Item\'s status successfully updated';
+        } else {
+            $message = 'Cannot update item\'s status';
+        }
+
+        return redirect()->back()->with('message', $message);
     }
 }
