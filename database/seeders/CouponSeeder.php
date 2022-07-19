e44b607b9b37a6c29cb973295588f1dc34967ba3
Exaltation <michstan96@gmail.com>
2022-07-19 15:04:47 +0700

    Add extra coupon to seeder

--- database/seeders/CouponSeeder.php
+++ database/seeders/CouponSeeder.php
@@ -18,17 +18,38 @@ class CouponSeeder extends Seeder {
             [
                 'code' => 'AUSPICIOUS10',
                 'exp_date' => Carbon::now()->addYears(1),
-                'amount' => 1000000,
+                'amount' => 100000,
                 'created_at' => $now,
                 'updated_at' => $now
             ],
             [
                 'code' => 'AUSPICIOUSX',
                 'exp_date' => Carbon::now()->addYears(1),
+                'amount' => 1000000,
+                'created_at' => $now,
+                'updated_at' => $now
+            ],
+            [
+                'code' => 'EXPIREDCODE',
+                'exp_date' => Carbon::now()->subYears(1),
+                'amount' => 100000000,
+                'created_at' => $now,
+                'updated_at' => $now
+            ],
+            [
+                'code' => 'MDPYV',
+                'exp_date' => Carbon::now()->addYears(1),
                 'amount' => 2000000,
                 'created_at' => $now,
                 'updated_at' => $now
-            ]
+            ],
+            [
+                'code' => 'NEWYEAR2023',
+                'exp_date' => Carbon::now()->addYears(1),
+                'amount' => 223000,
+                'created_at' => $now,
+                'updated_at' => $now
+            ],
         ]);
     }
 }
