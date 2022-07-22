70a74fdc157aa39c1b9ea0b7372491cc4b9188da
Daniel <60413613+arville27@users.noreply.github.com>
2022-07-22 18:33:41 +0700

    Fix pop up still visible on JS notif card

--- resources/js/cart.js
+++ resources/js/cart.js
@@ -76,6 +76,7 @@ function addToCart(button) {
                 toast = document.querySelector("#toast");
                 setTimeout(() => {
                     toast.style.opacity = 0;
+                    toast.style.visibility = "hidden";
                 }, 1500);
             });
         }
