194548dc8ce4bce80d66b34be6d89cc21e4e37f3
Exaltation <michstan96@gmail.com>
2022-07-22 15:21:38 +0700

    Make toast notification visibility hidden

--- resources/js/toast-notification.js
+++ resources/js/toast-notification.js
@@ -3,5 +3,6 @@ const toast = document.querySelector("#toast") ?? false;
 if (toast) {
     setTimeout(() => {
         toast.style.opacity = 0;
+        toast.style.visibility = "hidden";
     }, 1500);
 }
