eec97aa938a37c41c52bce56fc3c788a5791058d
Exaltation <michstan96@gmail.com>
2022-07-24 11:19:38 +0700

    Change placeholder searchbar language to english

--- resources/views/partials/searchbar.blade.php
+++ resources/views/partials/searchbar.blade.php
@@ -1,7 +1,7 @@
 <div>
     <form action="{{ route('search') }}" id="searchForm">
         <div class="rounded-md bg-gray-100 text-base py-3 px-5 relative border-2 hover:border-slate-300">
-            <input type="text" name="keyword" id="query" placeholder="Cari Produk, Judul Buku, Penulis"
+            <input type="text" name="keyword" id="query" placeholder="Search Product (Book Title, Author)"
                 autocomplete="off" class="bg-gray-100 outline-none w-full">
             <i class="flex fa-solid fa-magnifying-glass absolute right-3 text-xl"></i>
         </div>
